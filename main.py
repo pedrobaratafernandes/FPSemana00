b6964a831e8553d5167a57967526c0e35927f6be
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-11-04 16:51:23 +0000

    fix tuple to list

--- main.py
+++ main.py
@@ -28,10 +28,9 @@ print("valor de ouro: " + str(valor_moeda_ouro) + "valor da prata: " + str(valor
 valores = valor_moeda_ouro + valor_moeda_bronze + valor_moeda_prata
 print("valor total: " + str(valores))
 
-
 scores = ("Ana", "Bruno", "Carla", "David", "Eva")
-primeiros = scores[:2]
-outros = scores[2:]
+primeiros = list(scores[:2])
+outros = list(scores[2:])
 
 print(primeiros)
 print(outros)
